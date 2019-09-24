99c1433baae8718f0efe00e44d81a5789b03628c
Alfredo Arreola <alfredo@sfleet.mx>
2019-09-24 13:18:40 -0500

    Initial Commit

---
+++ sfleet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sfleet
+//
+//  Created by Alfredo Arreola on 9/24/19.
+//  Copyright © 2019 Alfredo Arreola. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
